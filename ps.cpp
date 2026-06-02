18802b28126284f4c6314353421710809fe23e06
youssef khaled <ykhaled@citglobal.com>
2026-06-02 13:41:04 +0300

    problem

--- ps.cpp
+++ ps.cpp
@@ -1,38 +1,49 @@
 #include <iostream>
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+// #include <bits/stdc++.h>
+#include <vector>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
 #define youssef ios_base::sync_with_stdio(0); cin.tie(0), cout.tie(0);
 #define el '\n'
 #define EL '\n'
 #define yes "YES"
 #define no "NO"
 #define ll long long
-const ll mod = 998244353 , infi = LONG_MAX;
+const ll mod = 998244353 , infi = 1e18;
 int N = 5000002 , loog = 30;
 using namespace std;
 #define int long long
 
-void solve()
-{
-    int n, k ;cin >> n >> k ;
+vector<vector<int>> dp ;
 
-    vector<int> ar(n) ;
-    vector<vector<int>> need(n , vector<int>(64,0)) ;
-    for (int i = 0 ; i < n; i++) {
-        cin >> ar[i] ;
-        bitset<64> x(ar[i]) ;
+int dfs(int idx , int rem, vector<int> &ar , vector<int> &price) {
+    if (rem <= 0)return 0 ;
 
-        for (int j = 0 ; j< 64;j++) {
-            int y = ((1<<j+1) -1) ;
+    if (idx < 0)return infi;
 
-            int tmp = ar[i] & ((1<<j+1) - 1) ;
-            need[i][j] = y - tmp ;
-            cout << need[i][j] << ' ' ;
-        }cout << el;
+    if (dp[idx][rem] != -1)return dp[idx][rem] ;
+
+    int ret = infi ;
+    for (int i = 1 ; i <= min(ar[idx] , rem) ; i++) {
+        ret = min(ret , dfs(idx -1 , rem-i , ar, price) + (i+10) * price[idx]) ;
     }
 
+    return dp[idx][rem]= ret ;
+}
 
+
+
+void solve() {
+    int n ;cin >> n ;
+
+    vector<int> ar(n) , prefx(n) , price(n) ;
+    for (int i =0 ; i< n;i++) {
+        cin >> ar[i] >> price[i] ;
+
+        prefx[i] = (i == 0)? ar[i] : ar[i] + prefx[i-1] ;
+    }
+    dp.assign(n , vector<int>(prefx[n-1] + 1 , -1)) ;
+    cout << dfs(n-1, prefx[n-1] , prefx, price) << el ;
 }
 
 signed main()
@@ -47,7 +58,8 @@ signed main()
 
     // preprocess();
 
-    // cin >> ts;
+    cin >> ts;
+
     for (int i = 1; i <= ts; i++)
     {
         solve();
