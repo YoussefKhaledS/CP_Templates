d91814782f37446b0f84ad17b13d7cb0e26fa5cd
Youssef_khaled <youssef.khaled56789@gmail.com>
2026-06-03 21:37:27 +0300

    added a very strong function to bitmanipulation.cpp

--- numbertheory/bitmanipulation.cpp
+++ numbertheory/bitmanipulation.cpp
@@ -24,3 +24,16 @@ int count1(int x , int b) { // return number of 1s from 0 to x for a bit index
     int ones = full * (1<<b) ;
     return ones+max(0ll, rem-(1<<b)) ;
 }
+
+int count1withsuffx(int x, int b, int pattern, int k) { // # of numbers n (0<=n<=x) such bit b in n is 1 and last k bits os n equal pattern
+    if (x < pattern)return 0 ;
+
+    int cnt = (x-pattern)/ (1ll<<k) +1 ;
+
+    if (b < k)
+        return ((pattern >> b) & 1)* cnt;
+
+    b-=k;
+    int len = 1ll << (b+1) ;
+    return (cnt/len) * (1ll << b) + max(0ll, cnt% len - (1ll <<b)) ;
+}
