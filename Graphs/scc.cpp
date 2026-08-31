8391a86daae9a35c91295f41517c12d4b01d69cc
youssef khaled <ykhaled@citglobal.com>
2026-08-31 17:25:45 +0300

    .

---
+++ Graphs/scc.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#define youssef ios_base::sync_with_stdio(0); cin.tie(0), cout.tie(0);
+#define el '\n'
+#define EL '\n'
+#define yes "YES"
+#define no "NO"
+#define ll long long
+const ll mod = 1e9 + 7 , infi = 1e17;
+int N = 500002 , loog = 40;
+using namespace std;
+using namespace __gnu_pbds;
+#define int long long
+// #define int uint64_t
+// #define u128 __uint128_t
+
+// strongly connected component -> self-contaned cycles in a directed graph
+
+// low-link value -> smallest node id reachable from current node lowlink[node] = smallest node id reachable from node
+
