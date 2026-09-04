418b96f6edede672a38ced1e7f43ec4e0243484f
Youssef_khaled <youssef.khaled56789@gmail.com>
2026-09-04 20:41:38 +0300

    .

--- Graphs/mst.cpp
+++ Graphs/mst.cpp
@@ -36,8 +36,91 @@ multiple and self loops are allowd
 
 Kruskal with Disjoint Set Union steps
 
-1- sort edges by ascending edge weight
-2- 
+1- sort edges by increasing edge weight
+2- initialize DSU for the graph
+3- for each edge do a union find to merge them if the are diff
 
 */
+struct DSU {
+    vector<int> parent, sz;
 
+    DSU(int n ) {
+        parent.resize(n+1);
+        sz.assign(n+1, 1) ;
+
+        for (int i = 1; i <= n; i++)
+            parent[i] =i;
+    }
+
+    int find(int x) {
+        if (parent[x] == x)return x;
+        return parent[x] = find(parent[x] );
+    }
+
+    void unite(int a, int b) {
+        a = find(a) ;
+        b = find(b) ;
+
+        if (a== b)return ;
+
+        if (sz[a] < sz[b])swap(a,b);
+
+        parent[b]= a;
+
+        sz[a]+=sz[b] ;
+        return ;
+    }
+
+    bool same(int a, int b) {
+        return find(a) == find(b);
+    }
+
+    int size(int x) {
+        return sz[find(x)] ;
+    }
+};
+
+struct Edge {
+    int u,v , weight;
+    bool operator<(Edge const& other) {
+        return weight < other.weight; // to sort asc
+    }
+};
+vector<vector<pair<int,int>>> mstAdj ;
+int mstCost  ;
+void mst(vector<vector<pair<int,int>>> &adj) {
+    int n = adj.size();
+
+    // step 1 createing the sorted edges array
+    vector<Edge> edges;
+    for (int node = 1 ; node < n ;node++) {
+
+        for (auto [child, w] : adj[node]) {
+            if (node < child)// because the adj is undirected and we need to store one edge
+                edges.push_back({node, child, w}) ;
+        }
+    }
+    sort(edges.begin(),edges.end());
+
+    //step2 init the dsu
+    DSU dsu(n) ;
+
+    mstAdj.assign(n, {});
+    int edgesCnt = 0;
+    mstCost= 0 ;
+
+    //step 3 kruskal
+    for (auto [u, v, w]: edges) {
+        if (dsu.same(u,v))continue;
+
+        dsu.unite(u,v);
+
+        mstAdj[u].push_back({v,w});
+        mstAdj[v].push_back({u,w});
+
+        mstCost+=w;
+        edgesCnt++;
+
+        if (edgesCnt == n-2)break;
+    }
+}
