7fc7d32faffc28a81ef5ca50c0d6db37f7aa045b
Youssef_khaled <youssef.khaled56789@gmail.com>
2026-09-13 22:08:34 +0300

    .

--- Graphs/Floyd_Warshall.cpp
+++ Graphs/Floyd_Warshall.cpp
@@ -13,9 +13,26 @@ using namespace std;
 using namespace __gnu_pbds;
 #define int long long
 
+// dp[i][j] = shortest path from i to j using only vertices [0 ... k]
+// Base case k = -1: only direct edges (and i -> i = 0) are allowed.
+
+// Transition:
+// Either the shortest i -> j path does NOT use k:
+//      dp[i][j]
+// Or it uses k:
+//      dp[i][k] + dp[k][j]
+// So:
+//      dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])
+// Base case: shortest path using no intermediate vertices.
+// Direct edge i -> j, or 0 if i == j, otherwise INF.
+
 struct Floyd {
     vector<vector<int>> dist;// shortest path u -> v
     vector<vector<int>> nxt ;// next vertex on shortest path u->v
+    vector<int> ngstate;
+    // 0 = not affected by ngcycle
+    // 1 = affected
+    // 2 = inside ngcycle
     bool ngcycle = false ;
 };
 
@@ -27,6 +44,7 @@ Floyd floydwarshall(int n, vector<vector<pair<int,int>>> &adj) {
     bool &ngcycle = res.ngcycle ;
     dist.assign(n, vector<int>(n,infi)) ;
     nxt.assign(n, vector<int>(n, -1)) ;
+    res.ngstate.assign(n, 0) ;
 
     for (int i = 0 ; i< n;i++) {
         dist[i][i] = 0 ;
@@ -52,12 +70,28 @@ Floyd floydwarshall(int n, vector<vector<pair<int,int>>> &adj) {
             }
         }
     }
-    // ngative cycle exist if dist[i][i] < 0
-    for (int i = 0 ; i < n;i++)
-        if (dist[i][i] < 0) {
-            ngcycle = true;
-        }
 
+    // dist[k][k] < 0   -->  k is involved in a negative cycle
+    for (int k = 0 ; k < n;k++) {
+        if (dist[k][k] >= 0)continue;
+
+        res.ngcycle = true;
+        res.ngstate[k] = 2 ;
+
+
+        //i can reach the negative cycle through k
+        //j can be reached from the negative cycle through k
+        for (int i = 0 ; i < n;i++) {
+            if (dist[i][k] == infi)continue;
+
+            for (int j = 0 ; j < n; j++) {
+                if (dist[k][j] == infi)continue;
+
+                if (res.ngstate[i] == 0)res.ngstate[i] =1 ;
+                if (res.ngstate[j] == 0)res.ngstate[j] =1 ;
+            }
+        }
+    }
     return res ;
 }
 
